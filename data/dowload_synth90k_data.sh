8d2bc3d2a104665ffd3de2e3e07582efb3bd5e55
bai-shang <42727348+bai-shang@users.noreply.github.com>
2018-09-27 14:44:21 +0800

    Update dowload_synth90k_data.sh

--- data/dowload_synth90k_data.sh
+++ data/dowload_synth90k_data.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-# download s
+# download synth90k
 wget -c http://www.robots.ox.ac.uk/~vgg/data/text/mjsynth.tar.gz
 tar -xzvf mjsynth.tar.gz
 
+# create synth90k image_list.txt
+# xxx/xxx/1234_abcd_8888.jpg
 find ./mnt/ | xargs ls -d | grep jpg > image_list.txt
 
+# create
 python create_synth90k_tfrecord.py --image_dir '' --anno_file ./image_list.txt
 
 
