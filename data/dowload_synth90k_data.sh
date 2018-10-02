bf63a88aed626c89572a5a001099c109c8620a95
bai-shang <42727348+bai-shang@users.noreply.github.com>
2018-10-02 14:36:17 +0800

    Update dowload_synth90k_data.sh

--- data/dowload_synth90k_data.sh
+++ data/dowload_synth90k_data.sh
@@ -5,7 +5,7 @@ wget -c http://www.robots.ox.ac.uk/~vgg/data/text/mjsynth.tar.gz
 tar -xzvf mjsynth.tar.gz
 
 # create synth90k image_list.txt
-# xxx/xxx/1234_abcd_8888.jpg
+# xxx/xxx/1234_AbCd_8888.jpg
 find ./mnt/ | xargs ls -d | grep jpg > image_list.txt
 
 # create tfrecord
